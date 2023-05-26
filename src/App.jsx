237ed843f279de0f068f77a59ad6bc025023c8d5
Mikail Rokib <atandarokib@gmail.com>
2023-05-26 16:53:23 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
+import Tokenomics from "./Components/Tokenomics";
 
 const App = () => {
   return (
     <div className="min-h-screen w-full overflow-hidden bg-red-800 text-white">
       <Hero />
       <About />
+      <Tokenomics />
     </div>
   );
 };
---
+++ src/Components/Tokenomics.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Tokenomics = () => {
+  return (
+    <section>
+      <section className="py-[50px] w-full flex justify-center overflow-hidden items-center">
+        <div className="container flex-col gap-4 flex justify-center items-center overflow-hidden">
+          <h1 className="text-6xl  md:text-9xl font-Maus z-10">tokenomics</h1>
+          <ul className="list-disc text-2xl flex flex-col gap-5 mt-5">
+            <li>1% tax</li>
+            <li>6969420 supply</li>
+            <li>1% max tx</li>
+            <li>lp burt</li>
+          </ul>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Tokenomics;
